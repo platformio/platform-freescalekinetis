f894f0f26f72e1040652370c2e71e455bc96ace3
Valerii Koval <valeros@bigmir.net>
2018-02-11 23:17:06 +0200

    Add support for J-Link, CMSIS-DAP and BlackMagic tools for debugging and uploading

--- platform.py
+++ platform.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from platform import system
+
 from platformio.managers.platform import PlatformBase
 
 
@@ -19,3 +21,69 @@ class FreescalekinetisPlatform(PlatformBase):
 
     def is_embedded(self):
         return True
+
+    def get_boards(self, id_=None):
+        result = PlatformBase.get_boards(self, id_)
+        if not result:
+            return result
+        if id_:
+            return self._add_default_debug_tools(result)
+        else:
+            for key, value in result.items():
+                result[key] = self._add_default_debug_tools(result[key])
+        return result
+
+    def _add_default_debug_tools(self, board):
+        debug = board.manifest.get("debug", {})
+        upload_protocols = board.manifest.get("upload", {}).get(
+            "protocols", [])
+        if "tools" not in debug:
+            debug['tools'] = {}
+
+        # CMSIS-DAP / BlackMagic Probe
+        for link in ("blackmagic", "cmsis-dap", "jlink"):
+            if link not in upload_protocols or link in debug['tools']:
+                continue
+            if link == "blackmagic":
+                debug['tools']['blackmagic'] = {
+                    "hwids": [["0x1d50", "0x6018"]],
+                    "require_debug_port": True
+                }
+            
+            elif link == "cmsis-dap":
+                pyocd_target = debug.get("pyocd_target")
+                assert pyocd_target
+
+                debug['tools'][link] = {
+                    "onboard": True,
+                    "server": {
+                        "package": "tool-pyocd",
+                        "executable": "$PYTHONEXE",
+                        "arguments": [
+                            "pyocd-gdbserver.py", 
+                            "-t", 
+                            pyocd_target
+                        ]
+                    }
+                }
+            
+            elif link == "jlink":
+                assert debug.get("jlink_device"), (
+                    "Missed J-Link Device ID for %s" % board.id)
+                debug['tools'][link] = {
+                    "server": {
+                        "arguments": [
+                            "-singlerun",
+                            "-if", "SWD",
+                            "-select", "USB",
+                            "-device", debug.get("jlink_device"),
+                            "-port", "2331"
+                        ],
+                        "executable": ("JLinkGDBServerCL.exe"
+                                       if system() == "Windows" else
+                                       "JLinkGDBServer")
+                    }
+                }
+    
+        board.manifest['debug'] = debug
+        return board
